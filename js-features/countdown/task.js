20f832fb76ab5a2662a9f47cc332fabe9c0f4bf2
Игорь Лухт <9425185@mail.ru>
2023-05-03 22:24:37 +0300

    1 задача решена

---
+++ js-features/countdown/task.js
@@ -0,0 +1,11 @@
+const addText = function(){
+
+const output = document.getElementById('timer');
+    output.textContent -= 1;
+    if (output.textContent <= 0) {
+        alert('Вы победили в конкурсе!');
+        clearInterval(inervalID);
+    }
+}
+const inervalID = setInterval(addText, 100);   
+

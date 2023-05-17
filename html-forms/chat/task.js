bf3a605ba1446224a08cbb0bf61ab1f1f22acf57
Игорь Лухт <9425185@mail.ru>
2023-05-17 22:55:27 +0300

    2 задача решение

---
+++ html-forms/chat/task.js
@@ -0,0 +1,47 @@
+const chatWidget = document.querySelector('.chat-widget'),
+      messages = document.querySelector( '.chat-widget__messages' ),
+      chatInput = document.getElementById('chat-widget__input');
+
+let date = new Date();
+
+chatWidget.addEventListener('click', () => {
+    chatWidget.classList.add('chat-widget_active');
+});
+
+document.addEventListener('keydown', event => {
+    if (event.key === 'Enter' && chatInput.value !== '') {
+        messages.innerHTML += `
+        <div class="message message_client">
+            <div class="message__time">${date.getHours()}:${date.getMinutes()}</div>
+            <div class="message__text">
+        ${chatInput.value}
+        </div>        
+        `
+        chatInput.value = "";
+
+        messages.innerHTML += `
+        <div class="message">
+            <div class="message__time">${date.getHours()}:${date.getMinutes()}</div>
+            <div class="message__text">${botAnswer()}</div>
+        </div>
+        `
+        messages.lastElementChild.scrollIntoView();
+    }
+});
+
+function botAnswer() {
+    let botSays = [
+          "Добрый день! До свидания!",
+          "Таким, как вы, не отвечаем",
+          "Устал от вас...",
+          "Ничего мы вам не продадим",
+          "Не нравитесь вы мне",
+          "Где ваша совесть?",
+          "Я ушел пить чай",
+          "Хотелось бы вам ответить... А нет, не хотелось!",
+          "К сожалению  все операторы сейчас заняты! Не пишите нам больше!"
+        ],
+        random = Math.floor(Math.random() * botSays.length);
+  
+    return botSays[random];
+}

8bdd0a71ca4fd21b615fa7ace61bd59d905783b6
Игорь Лухт <9425185@mail.ru>
2023-05-22 22:41:51 +0300

    1 задача

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,26 @@
+const tooltips = document.querySelectorAll('.has-tooltip');
+const tooltip = document.createElement('div');
+
+tooltips.forEach(el => {
+    el.addEventListener('click', function (event) {
+        event.preventDefault();
+        const thisTarget = event.target;
+        const tooltipActive = thisTarget.querySelector('.tooltip');
+
+        if (!tooltipActive) {
+            tooltip.innerText = '';
+            const title = thisTarget.getAttribute('title');
+            
+            let coord = el.getBoundingClientRect();
+            let left = el.dataset.position === 'right' ? coord.left + (coord.right - coord.left) +'px': coord.left+'px';
+            let top = el.dataset.position === 'top' ? coord.top + (coord.top - coord.bottom) +'px' : coord.bottom+'px';
+           
+            tooltip.classList.add('tooltip', 'tooltip_active');
+            tooltip.insertAdjacentText("afterBegin",`${title}`);
+            tooltip.setAttribute('style',`left: ${left}; top: ${top}`);
+            thisTarget.insertAdjacentElement("afterBegin", tooltip);
+        } else {
+            tooltipActive.remove();
+        }
+    })
+});
